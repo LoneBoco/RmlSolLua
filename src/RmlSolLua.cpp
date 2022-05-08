809f6fc79c55b06dc3fd881e8b957dcb97364b78
John Norman <loneboco@gmail.com>
2022-05-07 18:28:13 -0700

    Added initialization function.

--- src/RmlSolLua.cpp
+++ src/RmlSolLua.cpp
@@ -1,33 +1,18 @@
 ﻿#include "RmlSolLua.h"
 
-#include "bind/bind.h"
-#include "plugin/SolLuaInstancer.h"
+#include <sol/sol.hpp>
+#include <RmlUi/Core.h>
+
+#include "src/plugin/SolLuaPlugin.h"
 
 
 namespace Rml::SolLua
 {
 
-	SolLuaPlugin::SolLuaPlugin(sol::state_view lua_state)
-		: m_lua_state{ lua_state }
-	{
-	}
-
-	int SolLuaPlugin::GetEventClasses()
-	{
-		return EVT_BASIC;
-	}
-
-	void SolLuaPlugin::OnInitialise()
-	{
-		document_element_instancer = std::make_unique<SolLuaDocumentElementInstancer>(m_lua_state);
-		event_listener_instancer = std::make_unique<SolLuaEventListenerInstancer>(m_lua_state);
-		Factory::RegisterElementInstancer("body", document_element_instancer.get());
-		Factory::RegisterEventListenerInstancer(event_listener_instancer.get());
-	}
-
-	void SolLuaPlugin::OnShutdown()
-	{
-		delete this;
-	}
+    void Initialise(sol::state_view* state)
+    {
+        if (state != nullptr)
+            ::Rml::RegisterPlugin(new SolLuaPlugin(*state));
+    }
 
 } // end namespace Rml::SolLua
---
+++ src/plugin/SolLuaPlugin.h
@@ -0,0 +1,40 @@
+﻿// RmlSolLua.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include "../RmlSolLua.h"
+
+#include <RmlUi/Lua/Header.h>
+#include <RmlUi/Core/Plugin.h>
+#include <RmlUi/Core/Platform.h>
+#include <sol/sol.hpp>
+
+#include <memory>
+
+
+namespace Rml::SolLua
+{
+
+    class SolLuaDocumentElementInstancer;
+    class SolLuaEventListenerInstancer;
+
+    class RMLUILUA_API SolLuaPlugin : public Plugin
+    {
+    public:
+        SolLuaPlugin(sol::state_view lua_state);
+
+    private:
+        int GetEventClasses() override;
+
+        void OnInitialise() override;
+
+        void OnShutdown() override;
+
+        std::unique_ptr<SolLuaDocumentElementInstancer> document_element_instancer;
+        std::unique_ptr<SolLuaEventListenerInstancer> event_listener_instancer;
+
+        sol::state_view m_lua_state;
+    };
+
+} // end namespace Rml::SolLua
